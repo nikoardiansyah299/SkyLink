ffb98403ddc722cfc50dd25892ba97eaa687463b
rfkyrhmnz <riefkyrahmana35@gmail.com>
2025-12-02 12:59:17 +0700

    Auto-sync: file changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+        URL::forceScheme('https');
+        }
         // Register admin middleware alias if router available
         if ($this->app->has('router')) {
             $router = $this->app->get('router');
