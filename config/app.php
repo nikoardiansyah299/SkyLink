f3053c7cbdf91ee04b37f8876e0a10840139f14b
rfkyrhmnz <riefkyrahmana35@gmail.com>
2025-12-03 12:00:47 +0700

    Auto-sync: file changes

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://capstone-dtei.um.ac.id/skylink/public'),
 
     /*
     |--------------------------------------------------------------------------
